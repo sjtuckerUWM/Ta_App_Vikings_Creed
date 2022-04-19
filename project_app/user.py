bbe037009df2b785c4d09305c9f1fa69042c1361
Milad <mmasoodi@uwm.edu>
2022-04-19 12:34:49 -0500

    finished user skeleton

--- project_app/user.py
+++ project_app/user.py
@@ -1,18 +1,16 @@
 # User
 # __init__(self)
 # __init__(self, id, email, password, fName, lName, address, phoneNum)
-# Id: int
-# Email: string
-# Password: string
-# fName: string
-# lName: string
+# id: int
+# email: string
+# password: string
+# name: string
 # address: string
-# phoneNum: string
+# phone: string
 # getters/setters: accesses and modifies data within user
 
 
 class User(object):
-  """Reduced fraction class with integer numerator and denominator."""
   def __init__(self, id=-1):
     #precondition:
     #postcondition:
@@ -23,12 +21,27 @@ class User(object):
     pass
   def getID(self):
     pass
-  def setID(self, ID):
+  def setID(self, id):
     pass
-  def getEmail(self)
+  def getEmail(self):
     pass
-  def setEmail(self, Email):
+  def setEmail(self, email):
     pass
-  def getPassword
+  def getPassword(self):
+    pass
+  def setPassword(self, password):
+    pass
+  def getName(self):
+    pass
+  def setName(self, name):
+    pass
+  def getAddress(self):
+    pass
+  def setAddress(self, address):
+    pass
+  def getPhone(self):
+    pass
+  def setPhone(self, phone):
+    pass
 
 
