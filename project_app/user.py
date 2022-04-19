cd6f52630f8a07106a3e88505ee917e1aa395cc2
Milad <mmasoodi@uwm.edu>
2022-04-19 12:21:01 -0500

    started user skeleton

---
+++ project_app/user.py
@@ -0,0 +1,34 @@
+# User
+# __init__(self)
+# __init__(self, id, email, password, fName, lName, address, phoneNum)
+# Id: int
+# Email: string
+# Password: string
+# fName: string
+# lName: string
+# address: string
+# phoneNum: string
+# getters/setters: accesses and modifies data within user
+
+
+class User(object):
+  """Reduced fraction class with integer numerator and denominator."""
+  def __init__(self, id=-1):
+    #precondition:
+    #postcondition:
+    pass
+  def __str__(self):
+    #precondition:
+    #postcondition:
+    pass
+  def getID(self):
+    pass
+  def setID(self, ID):
+    pass
+  def getEmail(self)
+    pass
+  def setEmail(self, Email):
+    pass
+  def getPassword
+
+
