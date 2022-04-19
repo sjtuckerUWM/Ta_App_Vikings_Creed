d84232526f200d3f3c096194db1de4ee96146634
Jamie <jamie215215@hotmail.com>
2022-04-19 12:38:21 -0500

    added test

--- project_app/Unit_Tests/Course_Tests.py
+++ project_app/Unit_Tests/Course_Tests.py
@@ -5,3 +5,5 @@ class CourseTestCase(unittest.TestCase):
     def setup(self):
         self.assertEqual(True, False)  # add assertion here
 
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
