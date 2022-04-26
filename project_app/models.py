508701e03e847215c0b4de0f83f3f208368949df
Milad <mmasoodi@uwm.edu>
2022-04-25 22:12:27 -0500

    models is fixed no longer gives errors when running

--- project_app/models.py
+++ project_app/models.py
@@ -17,14 +17,14 @@ class CourseModel(models.Model):
     course_id = models.AutoField(primary_key=True)
     dept_code = models.CharField(max_length=20)
     name = models.CharField(max_length=20)
-    assigned_instructor = models.ForeignKey(UserModel.user_id, on_delete=models.PROTECT)
-    assigned_tas = models.ManyToManyField(UserModel.user_id)
+    assigned_instructor = models.ForeignKey(UserModel, on_delete=models.PROTECT , related_name="instuctor")
+    assigned_tas = models.ManyToManyField(UserModel)
 
 
 class SectionModel(models.Model):
     section_id = models.AutoField(primary_key=True)
-    course = models.ForeignKey(CourseModel.course_id, on_delete=models.CASCADE)
+    course = models.ForeignKey(CourseModel, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     grader = models.BooleanField(default=False)
-    assigned_ta = models.ForeignKey(UserModel.user_id, on_delete=models.PROTECT)
+    assigned_ta = models.ForeignKey(UserModel, on_delete=models.PROTECT)
 
