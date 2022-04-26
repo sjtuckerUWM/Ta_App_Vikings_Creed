dbd7f82e33dad0418cbe3e3e9364946602e879dc
Milad <mmasoodi@uwm.edu>
2022-04-25 21:13:01 -0500

    driver account related methods written, not tested

--- project_app/Driver.py
+++ project_app/Driver.py
@@ -15,29 +15,46 @@ class Driver(object):
         pass
 
     def fillAccounts(self):
-        pass
 
-    def addAccount_(self, id, email, password, name, address, phoneNum, role):
+        things = list(map(str, User.objects))
+        for entry in things:
+            if (entry.role is 0):
+                self.accountList.append(Supervisor(entry.id, entry.email, entry.password, entry.name, entry.address, entry.phoneNum))
+
+            elif (entry.role is 1):
+                self.accountList.append(Instructor(entry.id, entry.email, entry.password, entry.name, entry.address, entry.phoneNum))
+            elif (entry.role is 2):
+                self.accountList.append(TA(entry.id, entry.email, entry.password, entry.name, entry.address, entry.phoneNum))
+
+    def logIn(self, email, password):
+        if User.email.__contains__(email):
+            if User.objects.get(email=email).password is password:
+                return 2
+            return 1
+        return 0
+
+    def addAccount(self, id, email, password, name, address, phoneNum, role):
         if(role is 0) :
             self.accountList.append(Supervisor(id, email, password, name, address, phoneNum))
+
         elif(role is 1) :
             self.accountList.append(Instructor(id, email, password, name, address, phoneNum))
         elif (role is 2):
             self.accountList.append(TA(id, email, password, name, address, phoneNum))
+        User(id, email, password, name, address, phoneNum, role)
+        User.save()
 
-
-
-    def deleteAccount_(self, accountIndex):
+    def deleteAccount(self, accountIndex):
+        User.objects.get(user_id=self.accountList.__getitem__(accountIndex)).delete()
+        User.save()
         self.accountList.pop(accountIndex)
 
-    def addCourse_(self, courseid, coursename):
+    def addCourse(self, courseid, coursename):
         # Precondition: Course parameters are valid
         # Postcondition: Course has been added to the courses list
         pass
 
-    def deleteCourse_(self,  courseId):
-    # Precondition: The course ID being passed in is of the correct data type
-    #Postcondition: Deletes course with matching ID from the database
+    def deleteCourse(self,  courseIndex):
         pass
 
 
