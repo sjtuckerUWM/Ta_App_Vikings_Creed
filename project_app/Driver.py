f905c1d5dd7b43d1177015eca9a276f8fe718437
Milad <mmasoodi@uwm.edu>
2022-04-25 21:39:52 -0500

    driver model names updated and views pages fixed and trying session based driver

--- project_app/Driver.py
+++ project_app/Driver.py
@@ -3,7 +3,7 @@ from project_app.instructor import Instructor
 from project_app.TA import TA
 from project_app.course import Course
 from project_app.section import Section
-from models import User, Course, Section
+from models import UserModel, CourseModel, SectionModel
 
 class Driver(object):
     def __init__(self):
@@ -11,12 +11,12 @@ class Driver(object):
         self.accountList = []
         self.courseList = []
         self.fillAccounts()
-
-        pass
+        # to see if logIn works VVV
+        self.addAccount(1, "email@a.com", "pass", "Test User", "USA", "123-456-7890", 0)
 
     def fillAccounts(self):
 
-        things = list(map(str, User.objects))
+        things = list(map(str, UserModel.objects))
         for entry in things:
             if (entry.role is 0):
                 self.accountList.append(Supervisor(entry.id, entry.email, entry.password, entry.name, entry.address, entry.phoneNum))
@@ -27,8 +27,8 @@ class Driver(object):
                 self.accountList.append(TA(entry.id, entry.email, entry.password, entry.name, entry.address, entry.phoneNum))
 
     def logIn(self, email, password):
-        if User.email.__contains__(email):
-            if User.objects.get(email=email).password is password:
+        if UserModel.email.__contains__(email):
+            if UserModel.objects.get(email=email).password is password:
                 return 2
             return 1
         return 0
@@ -41,12 +41,12 @@ class Driver(object):
             self.accountList.append(Instructor(id, email, password, name, address, phoneNum))
         elif (role is 2):
             self.accountList.append(TA(id, email, password, name, address, phoneNum))
-        User(id, email, password, name, address, phoneNum, role)
-        User.save()
+        UserModel(id, email, password, name, address, phoneNum, role)
+        UserModel.save()
 
     def deleteAccount(self, accountIndex):
-        User.objects.get(user_id=self.accountList.__getitem__(accountIndex)).delete()
-        User.save()
+        UserModel.objects.get(user_id=self.accountList.__getitem__(accountIndex)).delete()
+        UserModel.save()
         self.accountList.pop(accountIndex)
 
     def addCourse(self, courseid, coursename):
