6fb801a93e390837f8821f0d2e45a79dcad1407e
Sam <samuel414tucker@gmail.com>
2022-04-27 18:28:20 -0500

    URL for delete and class created for Delete Account

--- project/urls.py
+++ project/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from project_app.views import Login, Home, AccountManagement, AddAccount
+from project_app.views import Login, Home, AccountManagement, AddAccount, DeleteAccount
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Login.as_view()),
     path('home/', Home.as_view()),
     path('accounts/', AccountManagement.as_view()),
-    path('addAccount/', AddAccount.as_view())
+    path('addAccount/', AddAccount.as_view()),
+    path('deleteAccount/<int:id>', DeleteAccount.as_view())
 ]
