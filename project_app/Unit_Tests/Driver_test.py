e359035ea5b05511b7b4f83841d460cd9dc19a41
Jamie <jamie215215@hotmail.com>
2022-05-01 22:01:56 -0500

    Got the skeletons ready for course and section tests, completed all account unit tests. The course and section tests will only require slight modification once methods are done

--- project_app/Unit_Tests/Driver_test.py
+++ project_app/Unit_Tests/Driver_test.py
@@ -1,4 +1,3 @@
-import unittest
 from django.test import TestCase
 from project_app.Driver import Driver
 
@@ -71,6 +70,38 @@ class TestEditAccount(TestCase):
             a.editAccount("", "", "", "", "", "", "", "", "", 0)
 
 
+class TestDeleteAccount(TestCase):
+    def test_delete_Instructor(self):
+        a = Driver()
+        a.addAccount("24", "Instructor@test.com", "pass1234", "test", "USA", "123-456-7890", 1)
+        self.assertIsNone(a.deleteAccount(24))
+
+    def test_delete_TA(self):
+        a = Driver()
+        a.addAccount("3", "TA@test.com", "pass1234", "test", "USA", "123-456-7890", 2)
+        self.assertIsNone(a.deleteAccount(3))
+
+    def test_delete_Supervisor(self):
+        a = Driver()
+        a.addAccount("5", "Supervisor@test.com", "pass1234", "test", "USA", "123-456-7890", 0)
+        self.assertIsNone(a.deleteAccount(5))
+
+    def test_delete_wrong_type(self):
+        a = Driver()
+        with self.assertRaises(TypeError, msg="invalid argument"):
+            a.editAccount(1, 1)
+
+    def test_delete_too_few_args(self):
+        a = Driver()
+        with self.assertRaises(TypeError, msg="too few arguments"):
+            a.editAccount()
+
+    def test_delete_too_many_args(self):
+        a = Driver()
+        with self.assertRaises(TypeError, msg="too many arguments"):
+            a.editAccount("1", "1")
+
+
 # 6 strings added when done, int: courseId, string: courseName
 class TestAddCourse(TestCase):
     def test_Add_Course(self):
@@ -102,52 +133,91 @@ class TestAddCourse(TestCase):
             c.addCourse_(351, 361)
 
 
-class TestDeleteCourse(unittest.TestCase):
-    def setup(self):
+class TestDeleteCourse(TestCase):
+    def test_delete_course1(self):
+        a = Driver()
+        a.addCourse(351, "CS351")
+        self.assertIsNone(a.deleteCourse(351))
+
+    def test_delete_course2(self):
+        a = Driver()
+        a.addCourse(361, "CS361")
+        self.assertIsNone(a.deleteCourse(361))
+
+    def test_delete_course3(self):
+        a = Driver()
+        a.addCourse(337, "CS337")
+        self.assertIsNone(a.deleteCourse(337))
+
+    def test_too_many_args(self):
         with self.assertRaises(OverflowError, msg="too many arguments"):
             a = Driver()
             a.deleteCourse_(123, "CS361", 801)
+
+    def test_no_args(self):
         with self.assertRaises(TypeError, msg=" no argument"):
             b = Driver()
             b.deleteCourse_()
+
+    def test_invalid_args(self):
         with self.assertRaises(TypeError, msg=" invalid arguments"):
             c = Driver()
             c.deleteCourse_("id")
 
-    def test_default(self):
+
+class TestAddSection(TestCase):
+    def test_add_section1(self):
         a = Driver()
-        self.assertTrue(a.deleteCourse_(123))
+        self.assertEqual(a.addSection(361, "CS361"))
 
+    def test_add_section2(self):
+        a = Driver()
+        self.assertEqual(a.addSection(361, "CS361"))
 
-class TestaddSection(unittest.TestCase):
-    def setup(self):
+    def test_add_section3(self):
+        a = Driver()
+        self.assertEqual(a.addSection(361, "CS361"))
+
+    def test_too_many_args(self):
         with self.assertRaises(OverflowError, msg="too many arguments"):
             a = Driver()
-            a.addSection_(123, "CS361", 801)
+            a.addSection(123, "CS361", 801)
+
+    def test_no_args(self):
         with self.assertRaises(TypeError, msg=" no argument"):
             b = Driver()
-            b.addSection_()
+            b.addSection()
+
+    def test_invalid_args(self):
         with self.assertRaises(TypeError, msg=" invalid arguments"):
             c = Driver()
-            c.addSection_("id",123)
+            c.addSection("id", 123)
+
+
+class TestDeleteSection(TestCase):
+    def test_default(self):
+        a = Driver()
+        self.assertTrue(a.deletesection(361, "CS361"))
 
     def test_default(self):
         a = Driver()
-        self.assertTrue(a.addSection_(361,"CS361"))
+        self.assertTrue(a.deletesection(361, "CS361"))
 
+    def test_default(self):
+        a = Driver()
+        self.assertTrue(a.deletesection(361, "CS361"))
 
-class TestDeleteSection(unittest.TestCase):
-    def setup(self):
+    def test_too_many_args(self):
         with self.assertRaises(OverflowError, msg="too many arguments"):
             a = Driver()
-            a.deletesection_(123, "CS361", 801)
+            a.deletesection(123, "CS361", 801)
+
+    def test_no_args(self):
         with self.assertRaises(TypeError, msg=" no argument"):
             b = Driver()
-            b.deletesection_()
+            b.deletesection()
+
+    def test_invalid_args(self):
         with self.assertRaises(TypeError, msg=" invalid arguments"):
             c = Driver()
-            c.deletesection_("id",123)
-
-    def test_default(self):
-        a = Driver()
-        self.assertTrue(a.deletesection_(361,"CS361"))
+            c.deletesection("id", 123)
