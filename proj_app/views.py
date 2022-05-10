f4826a93e3ad950d5af3591afdb8b6e735f1fac5
Jamie <jamie215215@hotmail.com>
2022-05-10 17:15:13 -0500

    section does not work

--- proj_app/views.py
+++ proj_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views import View
 
-from proj_app.models import MyUserModel, CourseModel, Department
+from proj_app.models import MyUserModel, CourseModel, Department, SectionModel
 from classes.Driver import Driver
 
 
@@ -209,3 +209,8 @@ class AddCourse(View):
 class AssignToCourse(View):
     def get(self, request, id):
         return render(request, "mainTemplates/assignToCoursePage.html")
+
+class SectionManagement(View):
+    def get(self, request, id):
+        sections = list(SectionModel.objects.filter(course_id=id).all())
+        return render(request, "mainTemplates/sectionManagement.html", {"sections": sections, "id": id})
