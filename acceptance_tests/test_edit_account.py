b2f44e51f0305cb4323b09de3720c50f23ae2814
turkogl3 <turkogl3@uwm.edu>
2022-05-16 15:26:58 -0500

    Merge branch 'master' into Selo_2
    # Conflicts:
    #	db.sqlite3

--- acceptance_tests/test_edit_account.py
+++ acceptance_tests/test_edit_account.py
@@ -7,25 +7,26 @@ class MyTestEditAccount(TestCase):
 
     def setUp(self):
         self.client = Client()
-        self.user = MyUserModel.objects.create(user_id= 2, email= 'Instructor@test.com', password='pass1234', name='Test', address='USA', phone_number='123-456-7890', role=1)
+        self.user = MyUserModel.objects.create(user_id=1, email= 'Instructor@test.com', password='pass1234', name='Test', address='USA', phone_number='123-456-7890', role=1)
         self.user.save()
 
     def test_account_editing(self):
         response = self.client.post('/', {'email': self.user.email, 'password': self.user.password})
         self.assertEqual(response.url, '/home/')
 
-        resp = self.client.post(f'/editAccount/{self.user.user_id}', {'id':self.user.user_id, 'email': "Instructor@uwm.edu", 'password': self.user.password,'name': self.user.name, 'address': self.user.address,'phoneNum': self.user.phone_number, 'role': self.user.role}, follow=True)
-        #  self.assertEqual(resp.context['v_email'], "email doesn't match")
+        resp1 = self.client.get('/editAccount/1/')
+        resp2 = self.client.post('/editAccount/1/', {'id': '1', 'email': 'Instructor@test.com', 'name': 'Test', 'password': "passworddd", 'address': 'USA', 'phoneNum': '123-456-7890'})
+        print(resp2.context) # gives no context because the page redirected to /accounts/
+        self.assertEqual(resp2.url, '/accounts/')
+        # need to fetch the user from the database again since editAccount in driver deletes the old one and makes a new one
+        self.assertEqual("passworddd", MyUserModel.objects.get(user_id=1).password,"AccountEdit is working")
 
-        self.assertEqual("Instructor@uwm.edu",self.user.email ,"email doesn't match")
-
-
-    def test_account_editing_invalid(self):
+    def test_account_invalid_editing(self):
         response = self.client.post('/', {'email': self.user.email, 'password': self.user.password})
         self.assertEqual(response.url, '/home/')
 
+        resp1 = self.client.get('/editAccount/1/')
+        resp2 = self.client.post('/editAccount/1/', {'id': '1', 'email': 'Instructor@test.com', 'name': 'Test', 'password': "pass", 'address': 'USA', 'phoneNum': '123-456-7890'})
+        self.assertEqual(resp2.context['v_password'], "Invalid, 8 or more characters")
+        self.assertEqual("pass1234", MyUserModel.objects.get(user_id=1).password,"password not changed")
 
-        resp = self.client.post(f'/editAccount/{self.user.user_id}', {'role':2 }, follow=True)
-        self.assertEqual(resp.context['role'], "Instructor", "can't change the role")
-
-        #self.assertEqual("Instructor@uwm.edu","Instructor@uwm.edu" ,"email doesn't match")
