31ff9775dd2a092c94c9b27d49c6a0dfacc4b6fb
Jamie <jamie215215@hotmail.com>
2022-05-16 17:47:43 -0500

    updated

--- acceptance_tests/test_Add_Section.py
+++ acceptance_tests/test_Add_Section.py
@@ -14,22 +14,19 @@ class MyTestAddSection(TestCase):
         response = self.client.post('/', {'email': self.user.email, 'password': self.user.password})
         self.assertEqual(response.url, '/home/')
 
-        self.course2 = CourseModel.objects.create(course_id=3, dept_code="COMP SCI", name=" Intro to AI")
-        self.section2 = SectionModel.objects.create(course_id=3, section_id=901)
+        self.client.post('/addCourse/', {'id': 3, 'dep': "COMP SCI", 'name': " Intro to AI"})
 
-        resp = self.client.post(f'/addSection/{self.section2.section_id}', {'id': self.section2.section_id})
-        self.assertEqual(resp.url, '/addSection/901/')
+        resp = self.client.post(f'/addSection/{self.section.section_id}', {'id': self.section.section_id})
+        self.assertEqual(resp.url, '/addSection/801/')
 
         sections = SectionModel.objects.all()
-
-        self.assertEqual(len(sections), 2)
+        self.assertEqual(len(sections), 1)
 
     def test_course_invalid_add(self):
         response = self.client.post('/', {'email': self.user.email, 'password': self.user.password})
         self.assertEqual(response.url, '/home/')
 
-        resp = self.client.post(f'/addSection/', {'id': -3}, follow=True)
+        resp = self.client.post(f'/addSection/{self.section.section_id}', {'id': -3}, follow=True)
         sections = SectionModel.objects.all()
 
-        # there should be only one item from the setup
         self.assertEqual(len(sections), 1)
