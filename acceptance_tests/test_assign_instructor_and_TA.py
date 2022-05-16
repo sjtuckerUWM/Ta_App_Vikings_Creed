01a1873235813cbf1f7e7dca8ecf9c55175f0398
turkogl3 <turkogl3@uwm.edu>
2022-05-16 14:44:12 -0500

    Merge branch 'master' into Selo_2
    # Conflicts:
    #	db.sqlite3

--- acceptance_tests/test_assign_instructor_and_TA.py
+++ acceptance_tests/test_assign_instructor_and_TA.py
@@ -6,15 +6,15 @@ class MyTestAssignCourse(TestCase):
 
     def setUp(self):
         self.client = Client()
-        self.course = CourseModel.objects.create(course_id=2, dept_code="COMP SCI", name=" Intro to Programming")
-        self.user = MyUserModel.objects.create(user_id=0, email='Instructor@test.com', password='pass1234', name='Test', address='USA', phone_number='123-456-7890', role=0)
+        self.course = CourseModel.objects.create(course_id =2,dept_code="COMP SCI", name=" Intro to Programming")
+        self.user = MyUserModel.objects.create(user_id=1,email='Instructor@test.com', password='pass1234', name='Test', address='USA', phone_number='123-456-7890', role=0)
 
     def test_no_Instructor(self):
         response = self.client.post('/', {'email': self.user.email, 'password': self.user.password})
         self.assertEqual(response.url, '/home/')
 
-        resp = self.client.get('assignToCourse/', {'instructor': '', 'tas': ''})
-        # self.assertEqual(resp.context['error'], 'There is no instructor to assign')
+        resp = self.client.post(f'/assignToCourse/{self.course.course_id}/', {'instructor': '', 'tas': ''}, follow=True)
+        self.assertEqual(resp.context["instructorList"],[], "There is no instructor to assign")
         instructors = list(MyUserModel.objects.filter(role=1).all())
         self.assertEqual(len(instructors),0);
 
@@ -23,20 +23,20 @@ class MyTestAssignCourse(TestCase):
         response = self.client.post('/', {'email': self.user.email, 'password': self.user.password})
         self.assertEqual(response.url, '/home/')
 
-        resp = self.client.get('assignToCourse/', {'instructor': '', 'tas': ''})
+        resp = self.client.post(f'/assignToCourse/{self.course.course_id}/', {'instructor': '', 'tas': ''}, follow=True)
         tas = list(MyUserModel.objects.filter(role=2).all())
-        # self.assertEqual(resp.context['error'], 'There is no TA to assign')
+        self.assertEqual(resp.context["instructorList"], [], "There is no TA to assign")
         self.assertEqual(len(tas), 0);
 
     def test_course_assigningInstructor(self):
         response = self.client.post('/', {'email': self.user.email, 'password': self.user.password})
         self.assertEqual(response.url, '/home/')
 
-        self.user1 = MyUserModel.objects.create(user_id=1, email='Instructor@test.com', password='pass1234', name='Test', address='USA', phone_number='123-456-7890', role=1)
-        self.user2 = MyUserModel.objects.create(user_id=2, email='TA1@test.com', password='pass1234', name='Test', address='USA', phone_number='123-456-7890', role=2)
-        self.user3 = MyUserModel.objects.create(user_id=3, email='TA2@test.com', password='pass1234', name='Test', address='USA', phone_number='123-456-7890', role=2)
+        self.user1 = MyUserModel.objects.create(user_id=4,email='Instructor@test.com', password='pass1234', name='Test', address='USA', phone_number='123-456-7890', role=1)
+        self.user2 = MyUserModel.objects.create(user_id=2,email='TA1@test.com', password='pass1234', name='Test', address='USA', phone_number='123-456-7890', role=2)
+        self.user3 = MyUserModel.objects.create(user_id=3,email='TA2@test.com', password='pass1234', name='Test', address='USA', phone_number='123-456-7890', role=2)
 
         tas = list(MyUserModel.objects.filter(role=2).all())
 
-        resp = self.client.post('assignToCourse/', {'instructor': self.user, 'tas': tas})
-        # self.assertEqual(resp.context['instructorList'],self.user1, "Problem with assigning Instructor")
+        resp = self.client.post(f'/assignToCourse/{self.course.course_id}/', {'instructor': self.user1.user_id, 'tas': tas}, follow=True)
+        self.assertEqual(resp.context['instructorList'],list(MyUserModel.objects.filter(role=1).all()), "Problem with assigning Instructor")
--- acceptance_tests/test_edit_account.py
+++ acceptance_tests/test_edit_account.py
@@ -7,15 +7,25 @@ class MyTestEditAccount(TestCase):
 
     def setUp(self):
         self.client = Client()
-        self.user = MyUserModel.objects.create(user_id=1, email= 'Instructor@test.com', password='pass1234', name='Test', address='USA', phone_number='123-456-7890', role=1)
+        self.user = MyUserModel.objects.create(user_id= 2, email= 'Instructor@test.com', password='pass1234', name='Test', address='USA', phone_number='123-456-7890', role=1)
         self.user.save()
 
     def test_account_editing(self):
         response = self.client.post('/', {'email': self.user.email, 'password': self.user.password})
         self.assertEqual(response.url, '/home/')
 
+        resp = self.client.post(f'/editAccount/{self.user.user_id}', {'id':self.user.user_id, 'email': "Instructor@uwm.edu", 'password': self.user.password,'name': self.user.name, 'address': self.user.address,'phoneNum': self.user.phone_number, 'role': self.user.role}, follow=True)
+        #  self.assertEqual(resp.context['v_email'], "email doesn't match")
 
-        response = self.client.post('editAccount/', {'user_id': '1', 'email': 'Instructor@test.com', 'name': 'Test', 'password': 'pass1234', 'address': 'USA', 'phone_number': '123-456-7890','role': '1'})
-        self.user.password = 'nopass'
+        self.assertEqual("Instructor@uwm.edu",self.user.email ,"email doesn't match")
 
-        self.assertEqual("nopass", self.user.password,"AccountEdit is not working")
+
+    def test_account_editing_invalid(self):
+        response = self.client.post('/', {'email': self.user.email, 'password': self.user.password})
+        self.assertEqual(response.url, '/home/')
+
+
+        resp = self.client.post(f'/editAccount/{self.user.user_id}', {'role':2 }, follow=True)
+        self.assertEqual(resp.context['role'], "Instructor", "can't change the role")
+
+        #self.assertEqual("Instructor@uwm.edu","Instructor@uwm.edu" ,"email doesn't match")
