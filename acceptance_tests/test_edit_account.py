0a73e68c0405e9b9c16faedf9f44e71d1e1138d0
Milad <mmasoodi@uwm.edu>
2022-05-17 09:24:50 -0500

    fixed edit account test

--- acceptance_tests/test_edit_account.py
+++ acceptance_tests/test_edit_account.py
@@ -7,7 +7,7 @@ class MyTestEditAccount(TestCase):
 
     def setUp(self):
         self.client = Client()
-        self.user = MyUserModel.objects.create(user_id=1, email= 'Instructor@test.com', password='pass1234', name='Test', address='USA', phone_number='123-456-7890', role=1)
+        self.user = MyUserModel.objects.create(user_id=1, email= 'Instructor@test.com', password='pass1234', name='Test', address='USA', phone_number='123-456-7890', role=0)
         self.user.save()
 
     def test_account_editing(self):
@@ -17,7 +17,7 @@ class MyTestEditAccount(TestCase):
         resp1 = self.client.get('/editAccount/1/')
         resp2 = self.client.post('/editAccount/1/', {'id': '1', 'email': 'Instructor@test.com', 'name': 'Test', 'password': "passworddd", 'address': 'USA', 'phoneNum': '123-456-7890'})
         print(resp2.context) # gives no context because the page redirected to /accounts/
-        self.assertEqual(resp2.url, '/accounts/')
+        self.assertEqual(resp2.url, '/accounts')
         # need to fetch the user from the database again since editAccount in driver deletes the old one and makes a new one
         self.assertEqual("passworddd", MyUserModel.objects.get(user_id=1).password,"AccountEdit is working")
 
